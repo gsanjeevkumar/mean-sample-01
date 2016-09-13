64730be2323c197c3f94c399e7d4b11b901fca47
Sanjeev <sanjeev.kumargadamsetti@bluegreenvacations.com>
2016-09-13 16:52:02 -0400

    added grunt capabilities - with growl

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'src/<%= pkg.name %>.js',
+        dest: 'build/<%= pkg.name %>.min.js'
+      }
+    },
+    growl : {
+    	myMessage : {
+    		message : "Some message",
+    		title : "Notification Title"
+    	}
+    }
+  });
+
+  // Default task(s).
+  grunt.registerTask('default', ['growl']);
+
+  grunt.loadNpmTasks('grunt-growl');
+  grunt.registerTask('default', 'growl:myMessage');
+
+};
